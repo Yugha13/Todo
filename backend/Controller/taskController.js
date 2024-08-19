93a8de92d0560426e94a1f14be5512efc87a1091
Yugha13 <yughas83@gmail.com>
2024-08-19 14:19:25 +0530

    changed the update

--- backend/Controller/taskController.js
+++ backend/Controller/taskController.js
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 
 const addTask = async ( req, res ) => {
     const { task } = req.body;
-    console.log(task);
+    //console.log(task);
     const oldUser = await prisma.task.create({
        data: {
          title: task ,
@@ -17,22 +17,39 @@ const addTask = async ( req, res ) => {
          },
        },
     });
-    console.log(oldUser);
+    //console.log(oldUser);
     return res.json({ msg : `task- ${task} created` });
 }
 
 
 const editTask = async ( req, res ) => {
   const { taskId, newTask } = req.body;
-  await prisma.task.update({
-    where : {
-      id : taskId
-    },
-    data : {
-      title: newTask
+  try {
+    const isUser = await prisma.user.findFirst({
+      where : {
+        gmail : req.body.gmail
+      },
+      include : {
+        tasks : true
+      }
+    })
+    if(isUser){
+      await prisma.task.update({
+        where : {
+          id : taskId,
+          userId : isUser.id
+        },
+        data : {
+          title: newTask
+        }
+      })
+      return res.json({ msg : `task updated to ${newTask}`});
     }
-  })
-  return res.json({ msg : `task updated to ${newTask}`});
+  } catch (e) {
+    console.log(e);
+    
+    return res.json({ msg : "no task found" });
+  }
 }
 
 const showTask = async ( req, res ) => {
@@ -70,6 +87,7 @@ const deleteTask = async ( req, res ) => {
       });
       return res.json({ msg : "task deleted " });
     }
+    return res.json({ msg: "something went wrong" });
   } catch (e) {
     return res.json({ msg : "no task found" });
   }
